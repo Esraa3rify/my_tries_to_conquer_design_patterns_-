1b8a2e50727cec62a811d0bd86fbda42c459e0fc
Esraa Sayed Arify <59229510+Esraa3rify@users.noreply.github.com>
2023-07-29 03:56:06 +0300

    Update and rename factory to factory.java

---
+++ factory.java
@@ -0,0 +1,37 @@
+//The diagram for the program
+
+public class pay_credit : Payment {
+
+public void pay(){
+
+Console.write("pay credit");
+}
+
+
+}
+
+-----------------------------------------------------------------------
+
+public class pay_cash : Payment {
+
+public void pay(){
+
+Console.write("pay cash");
+}
+
+-----------------------------------------------------------------------
+
+public class TemplatePattern{
+
+  public Paymnet get_class(String object_name){
+
+    if(object_name.Equals("cash"))  return new pay_cash();
+
+    else
+      return pay_credit();
+
+  }
+  
+}
+
+}
